67fa6f434375001e01e8732475a501fdb4387940
xbme2 <1612108684@qq.com>
2024-08-23 10:41:43 +0800

    updata labs and pro

--- su20-labs/su20-lab-starter/lab01/ll_cycle.c
+++ su20-labs/su20-lab-starter/lab01/ll_cycle.c
@@ -3,5 +3,12 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if(head == NULL)  return 0;
+    node *tortoise = head ,*hare = head;
+    while(hare->next && hare->next->next){
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if(hare == tortoise) return 1;
+    }
     return 0;
 }
