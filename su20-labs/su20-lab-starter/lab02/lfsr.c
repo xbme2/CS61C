3f5ca866052f67b9a4ccc27a84eaf7c45bc9696b
xbme2 <1612108684@qq.com>
2024-08-26 23:05:21 +0800

    finish l2 ex2

--- su20-labs/su20-lab-starter/lab02/lfsr.c
+++ su20-labs/su20-lab-starter/lab02/lfsr.c
@@ -3,8 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lfsr.h"
+#define ZEROMASK 0x0001
+#define TWOMASK 0x0004
+#define THREEMASK 0x0008
+#define FIVEMASK 0x0020
 
-void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+void lfsr_calculate(uint16_t *reg)
+{
+    uint16_t zero = (*reg) & ZEROMASK;
+    uint16_t two = ((*reg) & TWOMASK) >> 2;
+    uint16_t three = ((*reg) & THREEMASK) >> 3;
+    uint16_t five = ((*reg) & FIVEMASK) >> 5;
+
+    uint16_t MSB = (((zero ^ two) ^ three) ^ five) << 15;
+    *reg = (((*reg) >> 1) & (~(0x0001 << 15))) | MSB;
 }
-
