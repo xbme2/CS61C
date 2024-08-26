69ec1e28551205201aa1ac99380713d5f9085c9d
xbme2 <1612108684@qq.com>
2024-08-26 22:36:18 +0800

    finish l2 ex1

--- su20-labs/su20-lab-starter/lab02/bit_ops.c
+++ su20-labs/su20-lab-starter/lab02/bit_ops.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "bit_ops.h"
-
+#define MASK 0xffffffff
+#define ONE 0x00000001
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
@@ -9,26 +10,24 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns
     // 0 or 1)
-    return (x>>n) && 0x00000001;
+    return (x>>n) & 0x00000001;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    unsigned nright = *x;
-    nright<<(31-n);
-    unsigned nleft = *x;
-    nleft>>(n);
-    v<<n;
-    (v||nleft)||nright;
-    *x = v;
+    unsigned original = *x;
+    unsigned nright = (MASK<<(n+1))&original;
+    unsigned nleft = (~(MASK<<n))&original;
+    *x = (((v<<n)|nright)|nleft);
 
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    unsigned temp = *x;
+    set_bit(x,n,get_bit(~temp,n));
 }
 
