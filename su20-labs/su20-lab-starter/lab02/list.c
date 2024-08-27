c5c48e23a652afb48628f4f685700dd182b531a0
xbme2 <1612108684@qq.com>
2024-08-27 10:11:29 +0800

    finish lab02

--- su20-labs/su20-lab-starter/lab02/list.c
+++ su20-labs/su20-lab-starter/lab02/list.c
@@ -3,19 +3,20 @@
 /* Add a node to the end of the linked list. Assume head_ptr is non-null. */
 void append_node (node** head_ptr, int new_data) {
 	/* First lets allocate memory for the new node and initialize its attributes */
-	/* YOUR CODE HERE */
-
+	node *newNode = malloc(sizeof(node));
+	newNode->val = new_data;
+	newNode->next = NULL;
 	/* If the list is empty, set the new node to be the head and return */
 	if (*head_ptr == NULL) {
-		/* YOUR CODE HERE */
+		*head_ptr = newNode;
 		return;
 	}
 	node* curr = *head_ptr;
-	while (/* YOUR CODE HERE */ != NULL) {
+	while (curr->next != NULL) {
 		curr = curr->next;
 	}
 	/* Insert node at the end of the list */
-	/* YOUR CODE HERE */
+	curr->next = newNode;
 }
 
 /* Reverse a linked list in place (in other words, without creating a new list).
@@ -25,10 +26,13 @@ void reverse_list (node** head_ptr) {
 	node* curr = *head_ptr;
 	node* next = NULL;
 	while (curr != NULL) {
-		/* INSERT CODE HERE */
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
 	}
 	/* Set the new head to be what originally was the last node in the list */
-	*head_ptr = /* INSERT CODE HERE */
+	*head_ptr = prev;
 }
 
 
