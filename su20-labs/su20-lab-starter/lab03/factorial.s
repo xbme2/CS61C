2d0651537e2225e853926d93c6878a9896a2accf
xbme2 <1612108684@qq.com>
2024-08-28 16:28:58 +0800

    finish l3 ex3

--- su20-labs/su20-lab-starter/lab03/factorial.s
+++ su20-labs/su20-lab-starter/lab03/factorial.s
@@ -21,4 +21,14 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+    addi t1, x0, 1 # t1 store the answer
+    beq a0, x0, exit # if (n==0)
+loop:    
+    mul t1, a0, t1
+    addi a0, a0, -1
+    bne a0, x0, loop
+exit:
+    add a0, x0, t1
+    jr ra
+
+
