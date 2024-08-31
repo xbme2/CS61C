209790a5482e8ffb0c03b34a137d49220d1cd7dd
xbme2 <1612108684@qq.com>
2024-08-31 20:09:19 +0800

    finish pro2 A1 A2

---
+++ su20-proj2-starter/src/argmax.s
@@ -0,0 +1,51 @@
+.globl argmax
+
+.text
+# =================================================================
+# FUNCTION: Given a int vector, return the index of the largest
+#	element. If there are multiple, return the one
+#	with the smallest index.
+# Arguments:
+# 	a0 (int*) is the pointer to the start of the vector
+#	a1 (int)  is the # of elements in the vector
+# Returns:
+#	a0 (int)  is the first index of the largest element
+#
+# If the length of the vector is less than 1, 
+# this function exits with error code 7.
+# =================================================================
+argmax:
+
+    # Prologue
+
+
+loop_start:
+    beq a1, x0, wrong_exit
+
+    lw t0, 0(a0)    # t0 stores max value
+    add t1, x0, x0  # t1 stores max index
+    addi t3, x0, 1
+    addi a0, a0, 4
+
+loop_continue:
+    lw t2, 0(a0)
+    bge t0, t2, no_change
+    add t0, x0, t2
+    add t1, x0, t3
+no_change:
+    addi a0, a0, 4
+    addi t3, t3, 1
+    bne t3, a1, loop_continue
+loop_end:
+    add a0, x0, t1
+
+    # Epilogue
+
+
+    ret
+
+
+wrong_exit:
+    addi a0, x0, 10
+    addi a1, x0, 7
+    ecall  #Exit 7
---
+++ su20-proj2-starter/src/relu.s
@@ -0,0 +1,43 @@
+.globl relu
+
+.text
+# ==============================================================================
+# FUNCTION: Performs an inplace element-wise ReLU on an array of ints
+# Arguments:
+# 	a0 (int*) is the pointer to the array
+#	a1 (int)  is the # of elements in the array
+# Returns:
+#	None
+#
+# If the length of the vector is less than 1, 
+# this function exits with error code 8.
+# ==============================================================================
+relu:
+    # Prologue
+
+
+loop_start:
+    beq a1, x0 , wrong_exit # If the length of the vector is less than 1, wrong exit; 
+    
+loop_continue:
+    lw t0, 0(a0)
+    blt x0,t0, max
+    sw x0, 0(a0)
+max:
+    addi a0, a0, 4
+    addi a1, a1, -1
+    bne a1, x0, loop_continue
+loop_end:
+    # Epilogue
+	ret
+
+wrong_exit:
+    addi a0, x0, 10
+    addi a1, x0, 8
+    ecall  #Exit 8
+
+
+#ecall
+    #exit   a0:10   a1:exitcode
+    #print  a0:11   a1:content
+    #malloc a0:9    a1:size
